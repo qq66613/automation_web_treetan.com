dfe71d9616ad063e6de9c4b396aa95355fad61eb
qq66613 <firizqikaismail74@gmail.com>
2025-02-21 11:07:35 +0700

    delete on .config

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    setupNodeEvents(on, config) {
+      // Tambahkan event listener jika perlu
+      return config;
+    }
+  },
+});
